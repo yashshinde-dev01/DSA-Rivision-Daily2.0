dc1b97471c918521d745b1011d1062f3e988db68
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-06-28 18:12:53 +0530

    Time: 7 ms (81.96%), Space: 47.8 MB (57.69%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,29 +1,27 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int n=piles.length;
-        int max=piles[0];
-        for(int i=0;i<n;i++){
-            if(piles[i]>max){
-                max=piles[i];
-            }
+        int max = piles[0];
+        for (int p : piles) {
+            if (p > max) max = p;
         }
-        int l=1;
-        int r=max;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            int hr=kokoeat(piles,mid);
-            if(hr<=h){
-                r=mid-1;
-            }else{
-                l=mid+1;
+
+        int l = 1, r = max;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long hr = kokoeat(piles, mid);
+            if (hr <= h) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
         return l;
     }
-    static int kokoeat(int[]nums,int k){
-        int speed=0;
-        for(int i=0;i<nums.length;i++){
-            speed+=Math.ceil(nums[i]+k-1)/k;
+
+    static long kokoeat(int[] nums, int k) {
+        long speed = 0;
+        for (int num : nums) {
+            speed += ((long) num + k - 1) / k;
         }
         return speed;
     }
