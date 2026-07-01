2dbbf0e9a131ffa6e0725750fb25b55181a84305
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-07-01 09:09:58 +0530

    Time: 11 ms (68.68%), Space: 50.1 MB (56.02%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -7,17 +7,17 @@ class Solution {
        } 
        int l=max;
        int r=sum;
-       int ans=max;
+    //    int ans=max;
        while(l<=r){
         int mid=l+(r-l)/2;
         if(CanShip(weights, days,mid)){
-            ans=mid;
+            // ans=mid;
             r=mid-1;
         }else{
             l=mid+1;
         }
        }
-       return ans;
+       return l;
     }
     private static boolean CanShip(int[]nums,int days,int cap){
         int day=1;
